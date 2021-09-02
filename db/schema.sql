88ed61677253b101757a5bef9eef027269e51c9f
mariasalomevillarreal <msvillarreal2021@gmail.com>
2021-09-01 19:11:00 -0500

    correct schema and seeds files

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,32 @@
-DROP TABLE IF EXISTS departments;
-DROP TABLE IF EXISTS roles;
-DROP TABLE IF EXISTS employees;
-
-USE challenge
+DROP DATABASE IF EXISTS challenge;
+CREATE DATABASE challenge;
 
 
+
+USE challenge;
+
+/* CREATE TABLE FOR DEPARTMENTS*/
 CREATE TABLE departments (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
     dept_name VARCHAR(30) NOT NULL
 );
 
+/* CREATE TABLE FOR ROLES*/
 CREATE TABLE roles (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL (10,2) NOT NULL,
-    department_id INTEGER,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (departments_id) REFERENCES departments(id)
 ); 
 
+/* CREATE TABLE FOR EMPLOYEES*/
 CREATE TABLE employees (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER,
+    role_id INTEGER NOT NULL,
     manager_id INTEGER,
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id)  ON DELETE CASCADE,
-    FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
+    FOREIGN KEY (roles_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id) 
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,29 +1,45 @@
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES 
-('John', 'Doe', 1 , 1),
-('Mike', 'Chan', 2 , 1),
-('Ashley', 'Rodriguez', 3, 2),
-('Kevin', 'Tupik', 4, 2 ),
-('Malia', 'Brown', 5, 2),
-('Sarah', 'Lourd', 6, 3),
-('Tom', 'Allen', 7 ,3),
-('Christian', 'Eckenrode', 8, 3);
+--Add employee information into table--
+INSERT INTO employees (first_name, last_name, manager_id, role_id)
+VALUES ('John', 'Doe', 1 , 1);
+INSERT INTO employees (first_name, last_name, manager_id, role_id)
+VALUES ('Mike', 'Chan', 2 , 1);
+INSERT INTO employees (first_name, last_name, manager_id, role_id)
+VALUES ('Ashley', 'Rodriguez', 3, 2);
+INSERT INTO employees (first_name, last_name, manager_id, role_id)
+VALUES ('Kevin', 'Tupik', 4, 2 );
+INSERT INTO employees (first_name, last_name, manager_id, role_id)
+VALUES ('Malia', 'Brown', 5, 2);
+INSERT INTO employees (first_name, last_name, manager_id, role_id)
+VALUES ('Sarah', 'Lourd', null, 3);
+INSERT INTO employees (first_name, last_name, manager_id, role_id)
+VALUES ('Tom', 'Allen', null ,3);
+INSERT INTO employees (first_name, last_name, manager_id, role_id)
+VALUES ('Christian', 'Eckenrode', null, 3);
 
-
+--Add roles to table--
 INSERT INTO roles (title, salary, department_id)
-VALUES
-('Sales Lead', 100000, 1),
-('Lead Engineer', 120000, 2),
-('Lawyer', 190000, 3),
-('Software Engineer', 120000, 2),
-('Salesperson', 80000, 1),
-('Accountant', 125000, 4),
-('Legal team lead', 250000, 3),
-('Lead Engineer', 150000, 2);
+VALUES ('Sales Lead', 100000, 1);
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Lead Engineer', 120000, 2);
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Lawyer', 190000, 3);
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Software Engineer', 120000, 2);
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Salesperson', 80000, 1);
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Accountant', 125000, 4);
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Legal team lead', 250000, 3);
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Lead Engineer', 150000, 2);
 
+--Add departments to table--
 INSERT INTO departments (dept_name)
-VALUES 
-('Sales'),
-('Engineering'),
-('Finance'),
-('Legal');
+VALUES ('Sales');
+INSERT INTO departments (dept_name)
+VALUES('Engineering');
+INSERT INTO departments (dept_name)
+VALUES('Finance');
+INSERT INTO departments (dept_name)
+VALUES('Legal');
